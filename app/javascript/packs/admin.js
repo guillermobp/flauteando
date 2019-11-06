c311b69d7ec0bf62cf4f45b28d3104e8997d34d3
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2019-11-06 12:54:39 -0300

    :children_crossing: previsualiza imagenes seleccionadas

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -53,4 +53,32 @@ document.addEventListener("turbolinks:load", async () => {
       }
     });
   }
+
+  for (const input of [...document.querySelectorAll('input[type=file]')]) {
+    input.addEventListener('change', async (e) => {
+      for (const f of e.target.files) {
+        const reader = new FileReader();
+        const control = e.target.closest('.control');
+
+        reader.onload = function() {
+          const image = document.createElement('img');
+          const previewsContainer = control.querySelector('.previews') || false;
+          const previewContainer = control.querySelector('.preview') || false;
+
+          image.src = reader.result;
+
+          if (previewsContainer) {
+            previewsContainer.appendChild(image);
+          }
+
+          if (previewContainer) {
+            previewContainer.innerHTML = null;
+            previewContainer.appendChild(image);
+          }
+        }
+
+        await reader.readAsDataURL(f);
+      }
+    });
+  }
 });
