42da7781a754caae6f42bb60cb24f3146d387b09
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2020-09-19 20:21:16 -0300

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#artistas'
+  # root 'home#artistas'
+  root 'home2#index'
 
   get '/admin', to: 'admin/encuentros#index', as: :admin
 
