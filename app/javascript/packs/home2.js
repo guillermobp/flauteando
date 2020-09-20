06b7d6844d0c8402a0c7c7ebc9b01bc2fca8bb7c
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2020-09-20 01:33:03 -0300

    wip: slider with grid

--- app/javascript/packs/home2.js
+++ app/javascript/packs/home2.js
@@ -5,3 +5,18 @@ window.addEventListener('scroll', async (e) => {
     image.style.marginTop = `-${parseInt(window.scrollY)}px`;
   });
 });
+
+window.addEventListener('')
+const slider = document.querySelector('.hero .slider');
+const sliderStep = 100;
+
+let sliderScroll = 0;
+
+setInterval(async () => {
+
+  console.log({slider, sliderScroll});
+
+  sliderScroll -= sliderStep;
+  slider.style.transform = `translate(${sliderScroll}%)`;
+
+}, 2000);
