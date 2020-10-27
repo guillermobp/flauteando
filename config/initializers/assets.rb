2b96fcf32ed3804b8b2d3b0263407c589f532700
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2020-10-27 15:15:03 -0300

    wip: fixing missing assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,10 +6,11 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-# Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('node_modules', 'bulma-calendar', 'dist', 'css', 'bulma-calendar.min.css')
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.paths << Rails.root.join('node_modules', 'bulma-calendar', 'dist', 'css', 'bulma-calendar')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(login.css admin.css home.css)
+Rails.application.config.assets.precompile += %w(login.css admin.css home.css bulma-calendar/dist/css/bulma-calendar.min.css)
+# Rails.application.config.assets.precompile += Rails.root.join('node_modules', 'bulma-calendar', 'dist', 'css', 'bulma-calendar.min.css')
